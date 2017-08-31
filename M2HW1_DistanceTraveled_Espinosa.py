330970c043a2b3ed7f9178e78a48ca205e6d5260
espinosc5014 <31522573+espinosc5014@users.noreply.github.com>
2017-08-31 18:21:51 -0400

    Add files via upload
    8/31/2017

---
+++ M2HW1_DistanceTraveled_Espinosa.py
@@ -0,0 +1,26 @@
+# CTI-110
+# M2HW1-Distance Traveled
+# Christopher Espinosa
+# 08/31/2017
+#
+
+print ("How many miles will a car get traveling at 70 mph in...\n")
+#set speed at static 70 mph
+speed = (70)
+
+#set time 
+time6 = (6)
+time10 = (10)
+time15 = (15)
+
+#calculates distances
+distance6 = (speed * time6)
+distance10 = (speed * time10)
+distance15 = (speed * time15)
+
+print ("6 hours:", distance6, "miles.")
+print ("10 hours:", distance10, "miles.")
+print ("15 hours:", distance15, "miles.")
+
+input ("\nPress Enter to Exit")
+
