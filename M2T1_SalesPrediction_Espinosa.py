4d2bdfe2fd07e41597890712ae23c77d02114993
espinosc5014 <31522573+espinosc5014@users.noreply.github.com>
2017-08-31 17:51:15 -0400

    Delete M2T1_SalesPrediction_Espinosa.py

--- M2T1_SalesPrediction_Espinosa.py
+++
@@ -1,9 +0,0 @@
-# CTI-110 
-# M2T1 - Sales Prediction 
-# Christopher Espinosa
-# 08-29-2017
-#
-total_sales = float(input('Enter the projected sales: ')) 
-estimated_profit = total_sales * .23 
-print ('The estimated annual profit for the sales amount of $', format(total_sales,',.2f'), 'is $', format(estimated_profit,',.2f'))
-input ("\n\nPress the enter key to exit.")
