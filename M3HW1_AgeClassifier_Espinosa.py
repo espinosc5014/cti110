efb459bc980331d2b916231d0cd409306bfc5852
espinosc5014 <31522573+espinosc5014@users.noreply.github.com>
2017-09-12 19:07:49 -0400

    Add files via upload

--- M3HW1_AgeClassifier_Espinosa.py
+++ M3HW1_AgeClassifier_Espinosa.py
@@ -4,20 +4,23 @@
 # 09-12-2017
 #
 
+def main():
 
-age = float(input("Please enter your age: "))
+    age = float(input("Please enter your age: "))
 
-infant = 1
-child = 12
-teenager = 19
+    infant = 1
+    child = 12
+    teenager = 19
 
-if age <= infant:
-    print("You are an infant.  How are you using a computer?")
-elif age > infant and age <= child:
-    print("You are a child.")
-elif age > child and age <= teenager:
-    print("You are a teenager.")
-else:
-    print("You are an adult.")
+    if age <= infant:
+        print("You are an infant.  How are you using a computer?")
+    elif age > infant and age <= child:
+        print("You are a child.")
+    elif age > child and age <= teenager:
+        print("You are a teenager.")
+    else:
+        print("You are an adult.")
 
-input("\n\nPress Enter to Exit Program")
+    input("\n\nPress Enter to Exit Program")
+
+main()
